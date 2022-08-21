8083bf65e29b6c4bdc49dfc5c9bae6d179bf2718
m1raclee4u <64864409+m1raclee4u@users.noreply.github.com>
2022-08-21 20:35:56 +0300

    правки

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,8 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   // CSS file in the project
-  '~/assets/css/main.css'
+  '~/assets/css/main.css',
+  '~/assets/css/fonts.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
