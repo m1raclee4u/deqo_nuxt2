211cd0e3605470a982b226c229041d2b97d13138
m1raclee4u <64864409+m1raclee4u@users.noreply.github.com>
2023-01-28 22:32:10 +0300

    Ссылки на странице футер

--- components/General/Footer.vue
+++ components/General/Footer.vue
@@ -4,32 +4,32 @@
             <div class="left">
                 <div class="text">
                     <h5>Получайте скидки</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam suscipit voluptate eveniet possimus facere optio quibusdam quidem </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
                 <div class="form">
                     <form action="">
                         <input type="email" name="" placeholder="Введите своей email" id="email">
                         <button type="submit">Отправить</button>
                     </form>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitm</p>
+                    <p>DEQO 2023</p>
                 </div>
             </div>
             <div class="right">
                 <div class="column">
                     <h5>Информация</h5>
                     <div class="links">
-                        <Nuxt-Link to="">Частые вопросы</Nuxt-Link>
-                        <Nuxt-Link to="">Правила участия</Nuxt-Link>
-                        <Nuxt-Link to="">Стать участником</Nuxt-Link>
-                        <Nuxt-Link to="">Виды карт</Nuxt-Link>
-                        <Nuxt-Link to="">Адреса магазинов</Nuxt-Link>
+                        <Nuxt-Link to="/delivery">Оплата и доставка</Nuxt-Link>
+                        <Nuxt-Link to="/refund">Обмен и возврат</Nuxt-Link>
+                        <Nuxt-Link class="todo" to="">Оферта</Nuxt-Link>
+                        <Nuxt-Link class="todo" to="">Политика конфиденциальности</Nuxt-Link>
+                        <Nuxt-Link class="todo" to="">Пользовательское соглашение</Nuxt-Link>
                     </div>
                 </div>
                 <div class="column">
                     <h5>Контакты</h5>
                     <div class="links">
-                        <Nuxt-Link to="">example@mail.ru</Nuxt-Link>
-                        <Nuxt-Link to="">+7 (822) 156-90-15</Nuxt-Link>
+                        <a href="mailto:example@mail.ru">example@mail.ru</a>
+                        <a href="tel:+7 (822) 156-90-15">+7 (822) 156-90-15</a>
                     </div>
                 </div>
             </div>
@@ -68,7 +68,7 @@ form {
     margin-bottom: 40px;
 }
 .left{
-    height: 100%;
+    height: 90%;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -79,15 +79,25 @@ form {
         flex-direction: column;
         gap: 20px;
         p{
+            font-weight: 400;
             font-size: 16px;
-            line-height: 24px;
+            line-height: 19px;
+
+            /* средний */
+
+            color: #A9A1A1;
         }
     }
 
     h5{
-        font-size: 37px;
+        font-weight: 600;
+        font-size: 36px;
+        line-height: 43px;
+
+        /* основной */
+
+        color: #685F5F;
         margin-bottom: 35px;
-        line-height: 42px;
     }
     .form{
         p{
@@ -101,8 +111,20 @@ form {
     font-size: 15px;
     letter-spacing: -0.001em;
     .column{
-    display: flex;
-    gap: 50px;
+        display: flex;
+        gap: 50px;
+        h5{
+            font-weight: 600;
+            font-size: 18px;
+            line-height: 21px;
+            color: #685F5F;
+        }
+        font-size: 16px;
+        line-height: 19px;
+
+        /* средний */
+
+        color: #A9A1A1;
     }
     .links{
         display: flex;
@@ -117,22 +139,26 @@ form {
 input[type=email] {
     background-color: inherit;
     min-width: 300px;
-    height: 70px;
+    height: 35px;
     border-radius: 4px;
-    color: white;
-    background: #A1A1A1;
-        padding-left: 20px;
-
+    color: #A9A1A1;
+    border-bottom: 1px solid #A1A1A1;
     &::placeholder{
-        color: white;
+        color: #A9A1A1;
         // padding-left: 20px;
     }
     
   }
 button {
     background-color: inherit;
+    font-weight: 400;
     font-size: 16px;
-    font-weight: 600;
+    line-height: 19px;
+    text-align: center;
+    cursor: pointer;
+    /* тёмный */
+
+    color: #1A1A1A;
 }
 
 @media (max-width:1200px) {
